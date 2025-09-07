fdab7a25d959b38bf038fcfbcf571abab2590f68
deepwithdarby <deepwithdarby@gmail.com>
2025-09-07 08:46:16 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://imgsteg.com'
+  const baseUrl = 'https://imgsteg.vercel.app'
 
   return [
     {
